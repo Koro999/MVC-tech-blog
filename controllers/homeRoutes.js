993b0e73eafc9590925632e9a7c3140df9fde2f8
Koro999 <chris.dang93@hotmail.com>
2023-07-25 19:59:59 -0400

    fixed homepage need to fix other routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,38 +3,34 @@ const { Post, Comment, User } = require("../models");
 const withAuth = require("../utils/auth");
 
 //Get all posts and JOIN with user data
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   try {
-    // grab all information inside post model
     const postData = await Post.findAll({
       include: [
         {
-          //grab name from the user model
           model: User,
-          attributes: ["name"],
+          attributes: ['name'],
         },
         {
-          //grab user name from the comment model
           model: Comment,
           include: {
             model: User,
-            attributes: ["name"],
+            attributes: ['name'],
           },
         },
       ],
       order: [
-        //arrange in order of date created, descending
-        ["date_created", "DESC"],
-      ],
-    });
+        ['date_created', 'DESC'],
+      ]
+});
 
     // Serialize data so the template can read it
-    const posts = postData.map((post) => project.get({ plain: true }));
+    const posts = postData.map((post) => post.get({ plain: true }));
 
     // Pass serialized data and session flag into template
-    res.render("homepage", {
+    res.render('homepage', {
       posts,
-      logged_in: req.session.logged_in,
+      logged_in: req.session.logged_in
     });
   } catch (err) {
     res.status(500).json(err);
@@ -42,28 +38,31 @@ router.get("/", async (req, res) => {
 });
 
 //grab posts by id
-router.get("/post/:id", withAuth, async (req, res) => {
+router.get('/post/:id', withAuth, async (req, res) => {
   try {
-    //grab one from post
-    const postData = await Post.findByPk(req.params.id, {
+    //const pageTitle = 'Posts';
+    const postData = await Post.findOne({
+      where: {
+        id: req.params.id
+      },
       include: [
         User,
         {
-          //grab from User model, include name
           model: Comment,
           include: [User],
         },
       ],
-      order: [["date_created", "DESC"]],
+      order: [
+        ['date_created', 'DESC'],
+      ]
     });
 
-    //check to see if postData exists, if truthy
     if (postData) {
       const post = postData.get({ plain: true });
 
-      res.render("single-post", {
+      res.render('single-post', {
         post,
-        logged_in: req.session.logged_in,
+        logged_in: req.session.logged_in
       });
     } else {
       res.status(404).end();
