ab22a89af4bd220688a78d335eb65be710365d13
Koro999 <chris.dang93@hotmail.com>
2023-07-26 08:43:23 -0400

    fixed database issue

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,14 +1,13 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-// sign up
+//signup
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create(req.body);
 
     req.session.save(() => {
       req.session.user_id = userData.id;
-      req.session.name = userData.name;
       req.session.logged_in = true;
 
       res.status(200).json(userData);
@@ -18,37 +17,32 @@ router.post('/', async (req, res) => {
   }
 });
 
-//login
+//sign in
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({
-      where: {
-        name: req.body.name,
-      },
-    });
+    const userData = await User.findOne({ where: { email: req.body.email } });
 
     if (!userData) {
       res
         .status(400)
-        .json({ message: 'Incorrect username or password, please try again' });
+        .json({ message: 'Incorrect email or password, please try again' });
       return;
     }
 
-    const validPassword = userData.checkPassword(req.body.password);
+    const validPassword = await userData.checkPassword(req.body.password);
 
     if (!validPassword) {
       res
         .status(400)
-        .json({ message: 'Incorrect username or password, please try again' });
+        .json({ message: 'Incorrect email or password, please try again' });
       return;
     }
 
     req.session.save(() => {
       req.session.user_id = userData.id;
-      req.session.name = userData.name;
       req.session.logged_in = true;
-
-      res.json({ userData, message: 'You are now logged in!' });
+      
+      res.json({ user: userData, message: 'You are now logged in!' });
     });
 
   } catch (err) {
@@ -56,7 +50,6 @@ router.post('/login', async (req, res) => {
   }
 });
 
-//logout
 router.post('/logout', (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,24 +3,23 @@ const { Post, Comment, User } = require("../models");
 const withAuth = require("../utils/auth");
 
 //Get all posts and JOIN with user data
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const postData = await Post.findAll({
       include: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ["name"],
         },
       ]
-});
-
+    });
     // Serialize data so the template can read it
     const posts = postData.map((post) => post.get({ plain: true }));
 
     // Pass serialized data and session flag into template
-    res.render('homepage', {
+    res.render("homepage", {
       posts,
-      logged_in: req.session.logged_in
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
@@ -28,48 +27,79 @@ router.get('/', async (req, res) => {
 });
 
 //grab posts by id
-router.get('/post/:id', async (req, res) => {
+router.get("/post/:id", async (req, res) => {
   try {
     const postData = await Post.findByPk(req.params.id, {
       include: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ["name"],
         },
       ],
     });
 
-      const post = postData.get({ plain: true });
+    const post = postData.get({ plain: true });
 
-      res.render('post', {
-        post,
-        logged_in: req.session.logged_in
-      });
+    res.render("post", {
+      post,
+      logged_in: req.session.logged_in,
+    });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-//withAuth to prevent access to route 
-router.get('/profile', withAuth, async (req, res) => {
+//withAuth to prevent access to route
+//dashboard route
+router.get("/dashboard", withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
+      attributes: { exclude: ["password"] },
       include: [{ model: Post }],
     });
 
     const user = userData.get({ plain: true });
 
-    res.render('profile', {
+    res.render("dashboard", {
       ...user,
-      logged_in: true
+      logged_in: true,
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
+/*
+// AFTER CLICK ON NEW POST BUTTON
+router.get('/new', withAuth, (req, res) => {
+  // what view should we send the client when they want to create a new-post? (change this next line) - DONE!
+  res.render('new-post', {
+    // again, rendering with a different layout than main! no change needed
+    layout: 'dashboard',
+  });
+});
 
+// WHEN WE CLICK ON THE POST ITSELF
+router.get('/edit/:id', withAuth, async (req, res) => {
+  try {
+    // what should we pass here? we need to get some data passed via the request body -DONE!
+    const postData = await Post.findByPk(req.params.id);
+
+    if (postData) {
+      // serializing the data
+      const post = postData.get({ plain: true });
+      // which view should we render if we want to edit a post?
+      res.render('edit-post', {
+        layout: 'dashboard',
+        post,
+      });
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.redirect('login');
+  }
+});*/
 
 //login route
 router.get("/login", (req, res) => {
@@ -88,7 +118,7 @@ router.get("/signup", (req, res) => {
     res.redirect("/dashboard");
     return;
   }
-  res.render('signup')
+  res.render("signup");
 });
 
 module.exports = router;
